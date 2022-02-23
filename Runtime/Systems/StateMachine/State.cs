547d67f4ffd5b66a9cfeeebc568bbddbee4c371a
Robert Edilber <robert.edilber@pm.me>
2022-02-23 14:16:56 -0500

    moved simple save in save folder, added state machine

---
+++ Runtime/Systems/StateMachine/State.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Daniell.Runtime.Systems.StateMachine
+{
+    /// <summary>
+    /// Represents a state that can be used by a controller
+    /// </summary>
+    public class State
+    {
+        /* ==========================
+         * > Events
+         * -------------------------- */
+
+        /// <summary>
+        /// Called when the next state is ready
+        /// </summary>
+        public event Action<State> OnNextStateReady;
+
+
+        /* ==========================
+         * > Methods
+         * -------------------------- */
+
+        /// <summary>
+        /// Begin the state
+        /// </summary>
+        public virtual void OnBeginState() { }
+
+        /// <summary>
+        /// Update the state
+        /// </summary>
+        public virtual void OnUpdateState() { }
+
+        /// <summary>
+        /// End the state
+        /// </summary>
+        public virtual void OnEndState() { }
+
+        /// <summary>
+        /// End the current state
+        /// </summary>
+        /// <param name="nextState">State to transition to</param>
+        protected virtual void Finish(State nextState)
+        {
+            OnNextStateReady?.Invoke(nextState);
+        }
+    }
+}
---
+++ Runtime/Systems/StateMachine/StateController.cs
@@ -0,0 +1,65 @@
+﻿namespace Daniell.Runtime.Systems.StateMachine
+{
+    /// <summary>
+    /// Handles updating and transitionning between states
+    /// </summary>
+    /// <typeparam name="T">Type of state</typeparam>
+    public class StateController<T> where T : State
+    {
+        /* ==========================
+         * > Private Fields
+         * -------------------------- */
+
+        /// <summary>
+        /// Current state for this controller
+        /// </summary>
+        public T CurrentState { get; private set; }
+
+
+        /* ==========================
+         * > Methods
+         * -------------------------- */
+
+        #region State Logic
+
+        /// <summary>
+        /// Set a new state as the current state in the controller
+        /// </summary>
+        /// <param name="newState">State to be set as current state</param>
+        public void SetState(State newState)
+        {
+            // Do not execute if both states are null
+            if (CurrentState == null || newState == null)
+            {
+                return;
+            }
+
+            // Unsubscribe from the old state
+            CurrentState.OnNextStateReady -= SetState;
+
+            // End current state if it exists
+            CurrentState.OnEndState();
+
+            if (newState != null)
+            {
+                // Subscribe to the new state
+                newState.OnNextStateReady += SetState;
+
+                // Begin the new state and set as current state
+                newState.OnBeginState();
+                CurrentState = (T)newState;
+            }
+        }
+
+        /// <summary>
+        /// Update the current state
+        /// </summary>
+        public void UpdateCurrentState()
+        {
+            // Update the current state
+            CurrentState.OnUpdateState();
+        }
+
+        #endregion
+    }
+}
