973c39e936783e7abc33fcb5c70e2e72fcb0296d
Robert Edilber <robert.edilber@pm.me>
2022-07-15 14:46:21 -0400

    fixed RuntimeRef Setter Editor

--- Editor/Systems/References/RuntimeReferenceSetterEditor.cs
+++ Editor/Systems/References/RuntimeReferenceSetterEditor.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 namespace FM.Editor.References
 {
-    [CustomEditor(typeof(RuntimeReferenceSetter), true)]
-    public class RuntimeReferenceSetterEditor : UnityEditor.Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            /*
-            base.OnInspectorGUI();
-            var targetReferenceSetter = (RuntimeReferenceSetter)target;
+	[CustomEditor(typeof(RuntimeReferenceSetter), true)]
+	public class RuntimeReferenceSetterEditor : UnityEditor.Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
 
-            if (targetReferenceSetter.Reference != null)
-            {
-                EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
+			/*
+			var targetReferenceSetter = (RuntimeReferenceSetter)target;
 
-                if (GUILayout.Button("Set"))
-                {
-                    targetReferenceSetter.SetReference();
-                }
+			if (targetReferenceSetter != null)
+			{
+				EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
 
-                if (GUILayout.Button("Clear"))
-                {
-                    targetReferenceSetter.ClearReference();
-                }
+				if (GUILayout.Button("Set"))
+				{
+					targetReferenceSetter.SetReference();
+				}
 
-                EditorGUILayout.EndHorizontal();
-            }*/
-        }
-    }
+				if (GUILayout.Button("Clear"))
+				{
+					targetReferenceSetter.ClearReference();
+				}
+
+				EditorGUILayout.EndHorizontal();
+			}*/
+		}
+	}
 }
