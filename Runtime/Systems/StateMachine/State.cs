7fa30e351803c78156288b63154941dd870ef6eb
Robert Edilber <robert.edilber@pm.me>
2022-10-05 19:53:22 +0200

    Added state controller to state's ctor

--- Runtime/Systems/StateMachine/State.cs
+++ Runtime/Systems/StateMachine/State.cs
@@ -2,47 +2,62 @@
 
 namespace FM.Runtime.Systems.StateMachine
 {
-    /// <summary>
-    /// Represents a state that can be used by a controller
-    /// </summary>
-    public class State
-    {
-        /* ==========================
+	/// <summary>
+	/// Represents a state that can be used by a controller
+	/// </summary>
+	public class State
+	{
+		/* ==========================
+		 * > Properties
+		 * -------------------------- */
+
+		/// <summary>
+		/// The <see cref="StateController{T}"/> controlling this state
+		/// </summary>
+		public StateController<State> StateController { get; }
+
+
+		/* ==========================
          * > Events
          * -------------------------- */
 
-        /// <summary>
-        /// Called when the next state is ready
-        /// </summary>
-        public event Action<State> OnNextStateReady;
+		/// <summary>
+		/// Called when the next state is ready
+		/// </summary>
+		public event Action<State> OnNextStateReady;
 
 
-        /* ==========================
+		/* ==========================
+		 * > Constructor
+		 * -------------------------- */
+
+		public State(StateController<State> stateController)
+		{
+			StateController = stateController;
+		}
+
+
+		/* ==========================
          * > Methods
          * -------------------------- */
 
-        /// <summary>
-        /// Begin the state
-        /// </summary>
-        public virtual void OnBeginState() { }
+		/// <summary>
+		/// Begin the state
+		/// </summary>
+		public virtual void OnBeginState() { }
 
-        /// <summary>
-        /// Update the state
-        /// </summary>
-        public virtual void OnUpdateState() { }
+		/// <summary>
+		/// End the state
+		/// </summary>
+		public virtual void OnEndState() { }
 
-        /// <summary>
-        /// End the state
-        /// </summary>
-        public virtual void OnEndState() { }
-
-        /// <summary>
-        /// End the current state
-        /// </summary>
-        /// <param name="nextState">State to transition to</param>
-        protected virtual void Finish(State nextState)
-        {
-            OnNextStateReady?.Invoke(nextState);
-        }
-    }
+		/// <summary>
+		/// End the current state
+		/// </summary>
+		/// <param name="nextState">State to transition to</param>
+		protected virtual void Finish(State nextState)
+		{
+			OnNextStateReady?.Invoke(nextState);
+		}
+	}
 }
