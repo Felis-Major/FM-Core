bb25e7a996d480479345e83786bf112d07b0c20f
Robert Edilber <robert.edilber@pm.me>
2022-10-30 10:59:25 +0100

    Added non generic IWeightedItem version

--- Runtime/Helpers/Random/Selectors/IWeightedItem.cs
+++ Runtime/Helpers/Random/Selectors/IWeightedItem.cs
@@ -1,21 +1,25 @@
-﻿using UnityEngine;
-
-namespace FM.Runtime.Helpers.Random
+﻿namespace FM.Runtime.Helpers.Random
 {
+    /// <summary>
+    /// Base class for a weighted item
+    /// </summary>
+    public interface IWeightedItem
+    {
+        /// <summary>
+        /// Weight of the item
+        /// </summary>
+        int Weight { get; }
+    }
+
     /// <summary>
     /// Interface for an item that can be randomly picked using weight
     /// </summary>
     /// <typeparam name="T">Value of the item</typeparam>
-    public interface IWeightedItem<T>
+    public interface IWeightedItem<T> : IWeightedItem
     {
         /// <summary>
         /// Value of the item
         /// </summary>
         T Value { get; }
-
-        /// <summary>
-        /// Weight of the item
-        /// </summary>
-        int Weight { get; }
     }
 }
