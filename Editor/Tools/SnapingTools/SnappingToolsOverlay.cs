8dc014042a8256846e52f31f528ce91529e26dde
Robert Edilber <robert.edilber@pm.me>
2022-11-17 17:34:28 +0100

    added invert bool in event receiver

---
+++ Editor/Tools/SnapingTools/SnappingToolsOverlay.cs
@@ -0,0 +1,9 @@
+using UnityEditor;
+using UnityEditor.Overlays;
+
+[Overlay(typeof(SceneView), "Snapping Tools")]
+public class SnappingToolsOverlay : ToolbarOverlay
+{
+
+}
+
--- Runtime/Systems/Events/Types/Base/BoolEventReceiver.cs
+++ Runtime/Systems/Events/Types/Base/BoolEventReceiver.cs
@@ -1,9 +1,35 @@
-﻿using FM.Runtime.Systems.Events;
+﻿using UnityEngine;
 
 namespace FM.Runtime.Systems.Events
 {
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
-    public class BoolEventReceiver : GenericEventReceiver<bool, BoolEvent> { }
+    public class BoolEventReceiver : GenericEventReceiver<bool, BoolEvent>
+    {
+        /* ==========================
+         * > Private Serialized fields
+         * -------------------------- */
+
+        [SerializeField]
+        [Tooltip("Should the value be inverted")]
+        private bool _shouldInvertValue;
+
+
+        /* ==========================
+         * > Methods
+         * -------------------------- */
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        protected override void OnEventReceived(bool value)
+        {
+            // Process incoming value
+            bool newValue = _shouldInvertValue ? !value : value;
+
+            // Execute the base method with the new value
+            base.OnEventReceived(newValue);
+        }
+    }
 }
